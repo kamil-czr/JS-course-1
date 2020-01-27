d9c10707a00bb161f37768d7222247c3c3da95f7
kamil-czr <k.czarnocki@gmail.com>
2020-01-28 00:12:39 +0100

    Tested break & continue in script.js

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 
-var links = document.getElementsByTagName("a");
 
-for (i = 1; i <= links.length; i++) {
 
-        console.log("this is link number" + i)
+for (i = 0; i < 10; i++) {
+
+    if(i === 5 || i === 3){
+
+        continue;
+    }
+
+        console.log(i)
         
+        if ( i === 7){
+            break;
+        }
 }
-
-document.write("all links looped"); 
