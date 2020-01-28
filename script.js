ca5ddf01787122a60a5350a39b55de542ea723ed
kamil-czr <k.czarnocki@gmail.com>
2020-01-28 19:34:47 +0100

    Tested constructor function in script.js

--- script.js
+++ script.js
@@ -1,20 +1,5 @@
-console
-
-var myArray = [];
-
-myArray[0] = 25;
-myArray[1] = "hello";
-
-var myCar = new Object();
-myCar.maxSpeed = 50;
-myCar.driver = "Kamil";
-myCar.drive = function(){
-    console.log("now driving first time");
-};
-
-myCar.drive();
-
-var myCar2 = {
+/*
+var myCar = {
     
     maxSpeed: 70, 
     driver: "Kamil", 
@@ -25,8 +10,32 @@ var myCar2 = {
     logDriver: function(){
         console.log("Driver name is " + this.driver);
     }
-};
+}; 
+*/
 
-myCar2.logDriver();
-console.log("Max speed is", myCar2.maxSpeed);
-myCar2.drive(50, 3);
+
+var Car = function(maxSpeed, driver){
+
+    this.maxSpeed = maxSpeed
+    this.driver = driver
+    this.drive = function(speed, time){
+        console.log("traveled distance is",speed * time);
+    };
+    this.logDriver = function(){
+        console.log("Driver name is " + this.driver);
+    };
+    this.logMaxSpeed = function(){
+        console.log("Max speed is " + this.maxSpeed);
+    };
+
+}
+
+var myCar = new Car(70, "Darek");
+var myCar2 = new Car(100, "Wojtek");
+var myCar3 = new Car(120, "Piotr");
+var myCar4 = new Car(90, "Marek");
+var myCar5 = new Car(110, "Dawid");
+
+myCar3.logMaxSpeed();
+myCar4.drive(30,5);
+myCar3.logDriver();
