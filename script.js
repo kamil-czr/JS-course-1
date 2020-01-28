349778ba13db69410551f1fc4e7d9e7926996dc1
kamil-czr <k.czarnocki@gmail.com>
2020-01-28 18:30:19 +0100

    Created objects in script.js

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
-var string1 = "abc";
-var string2 = "bcd";
+var myArray = [];
 
-console.log(string1 === string2);
+myArray[0] = 25;
+myArray[1] = "hello";
 
+var myCar = new Object();
+myCar.maxSpeed = 50;
+myCar.driver = "Kamil";
+myCar.drive = function(){
+    console.log("now driving first time");
+};
+
+myCar.drive();
+
+var myCar2 = {
+    
+    maxSpeed: 70, 
+    driver: "Kamil", 
+    drive: function(speed, time){
+        console.log("traveled distance is",speed * time);
+    }
+};
+
+console.log("Max speed is", myCar2.maxSpeed);
+myCar2.drive(50, 3);
