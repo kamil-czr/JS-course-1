a8ced0784b857a1d3657797ca8dd8443563a14ce
kamil-czr <k.czarnocki@gmail.com>
2020-01-29 23:52:53 +0100

    Tested timers in script.js

--- script.js
+++ script.js
@@ -1,28 +1,33 @@
-function setUpEvents(){
+/*var myMessage = document.getElementById("message");
 
-    var content = document.getElementById("content");
-    var button = document.getElementById("show-more");
-    
-    button.onclick = function(){
-    
-            if(content.className == "open"){
-                    //shrink the box
-                content.className = "";
-                button.innerHTML = "Show Less";
-            } else {
-                //expand the box
-                content.className = "open";
-                button.innerHTML = "Show Less";
-            }
-    
+function showMessage(){
 
-
-};
+    myMessage.className = "show";
 
 }
 
-window.onload = function(){
+setTimeout(showMessage, 3000); */
 
-    this.setUpEvents();
+var colorChanger = document.getElementById("color-changer");
 
-};
+var colors = ["red", "blue", "green", "pink"];
+var counter = 0;
+
+function changeColor(){
+
+    if (counter >= colors.length){
+        counter = 0;
+    }
+
+    colorChanger.style.background = colors[counter];
+    counter++;
+}
+
+var myTimer = setInterval(changeColor, 3000);
+
+colorChanger.onclick = function(){
+
+    clearInterval(myTimer);
+    colorChanger.innerHTML = "Timer stopped";
+
+}
