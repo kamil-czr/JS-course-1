9c1fd3ae9fe3afaf361bb2cdc1881f8ba0e1d3d4
kamil-czr <k.czarnocki@gmail.com>
2020-01-29 22:56:20 +0100

    Tested onLoad event in script.js

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
-var content = document.getElementById("content");
-var button = document.getElementById("show-more");
+function setUpEvents(){
 
-button.onclick = function(){
+    var content = document.getElementById("content");
+    var button = document.getElementById("show-more");
+    
+    button.onclick = function(){
+    
+            if(content.className == "open"){
+                    //shrink the box
+                content.className = "";
+                button.innerHTML = "Show Less";
+            } else {
+                //expand the box
+                content.className = "open";
+                button.innerHTML = "Show Less";
+            }
+    
 
-        if(content.className == "open"){
-                //shrink the box
-            content.className = "";
-            button.innerHTML = "Show Less";
-        } else {
-            //expand the box
-            content.className = "open";
-            button.innerHTML = "Show Less";
-        }
 
+};
 
 }
+
+window.onload = function(){
+
+    this.setUpEvents();
+
+};
