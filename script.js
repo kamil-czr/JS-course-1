55ffd34c68503f237699e33ebbe017cff9a04dcb
kamil-czr <k.czarnocki@gmail.com>
2020-01-30 14:20:48 +0100

    Tested form validation in script.js

--- script.js
+++ script.js
@@ -1,33 +1,14 @@
-/*var myMessage = document.getElementById("message");
+var myForm = document.forms.myForm;
+var message = document.getElementById("message");
 
-function showMessage(){
+myForm.onsubmit = function(){
 
-    myMessage.className = "show";
-
-}
-
-setTimeout(showMessage, 3000); */
-
-var colorChanger = document.getElementById("color-changer");
-
-var colors = ["red", "blue", "green", "pink"];
-var counter = 0;
-
-function changeColor(){
-
-    if (counter >= colors.length){
-        counter = 0;
+    if(myForm.name.value == ""){
+        message.innerHTML = "please enter your name";
+        return false;
+    } else {
+        message.innerHTML = "";
+        return true;
     }
 
-    colorChanger.style.background = colors[counter];
-    counter++;
-}
-
-var myTimer = setInterval(changeColor, 3000);
-
-colorChanger.onclick = function(){
-
-    clearInterval(myTimer);
-    colorChanger.innerHTML = "Timer stopped";
-
-}
+};
